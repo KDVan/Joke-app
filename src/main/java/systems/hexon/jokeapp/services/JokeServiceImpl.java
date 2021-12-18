f97edc2f5cf44fe57841a68a626af493aaa959f6
VanBaKhanhDuy <duyvbkh134@gmail.com>
2021-12-18 11:00:46 +0700

    Fixed: [Get Joke] Function Return Null Committed on Sat. Dec 18th, 2021 Close #12

--- src/main/java/systems/hexon/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/systems/hexon/jokeapp/services/JokeServiceImpl.java
@@ -17,13 +17,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
 
     public JokeServiceImpl() {
-        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
     public String getJoke() {
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
